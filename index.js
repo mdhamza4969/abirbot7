4f150f857e03493df0232e52e92bec7f88cd61ef
SABBIR HOSSEIN <sabbirabir606@gmail.com>
2025-11-27 22:24:39 +0600

    Enhance author details and fix Goat Bot deployment
    Updated author information and added a deployment fix for Goat Bot.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 /**
- * @author NTKhang
+ * @author NTKhang enhanced by SABBIR HOSSEIN
  * ! The source code is written by NTKhang, please don't change the author's name everywhere. Thank you for using
  * ! Official source code: https://github.com/ntkhang03/Goat-Bot-V2
+ * ! enhanced source code:
+https://github.com/sabbirmaghla/SABBIR-Harny
  * ! If you do not download the source code from the above address, you are using an unknown version and at risk of having your account hacked
  *
  * English:
@@ -9,30 +11,39 @@
  * It is my motivation to maintain and develop the project for free.
  * ! If you change it, you will be banned forever
  * Thank you for using
- *
- * Vietnamese:
- * ! Vui lòng không thay đổi mã bên dưới, nó rất quan trọng đối với dự án.
- * Nó là động lực để tôi duy trì và phát triển dự án miễn phí.
- * ! Nếu thay đổi nó, bạn sẽ bị cấm vĩnh viễn
- * Cảm ơn bạn đã sử dụng
+ * Goat Bot Render Deployment Fix by SABBIR HOSSEIN
  */
 
+const express = require("express");
 const { spawn } = require("child_process");
 const log = require("./logger/log.js");
 
-function startProject() {
-	const child = spawn("node", ["Goat.js"], {
-		cwd: __dirname,
-		stdio: "inherit",
-		shell: true
-	});
+// === Express server to keep Render service alive ===
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-	child.on("close", (code) => {
-		if (code == 2) {
-			log.info("Restarting Project...");
-			startProject();
-		}
-	});
+app.get("/", (req, res) => {
+        res.send("EREN BOT RUNNING \n author: Eren \n Status: smooth ");
+});
+
+app.listen(PORT, () => {
+        console.log(`✅ | SABBIR HOSSEIN Server running at http://localhost:${PORT}`);
+});
+
+// === Start the Goat bot process ===
+function startProject() {
+        const child = spawn("node", ["Goat.js"], {
+                cwd: __dirname,
+                stdio: "inherit",
+                shell: true
+        });
+
+        child.on("close", (code) => {
+                if (code === 2) {
+                        log.info("Restarting Project...");
+                        startProject();
+                }
+        });
 }
 
 startProject();
