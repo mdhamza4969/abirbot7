885d68e96ddeca1e1def240697983b5d7d9d5f57
mdhamza4969 <md.hamza.4969@gmail.com>
2025-12-15 18:00:41 +0600

    Update tag.js

--- scripts/cmds/tag.js
+++ scripts/cmds/tag.js
@@ -4,28 +4,28 @@ module.exports = {
     version: "1.4",
     author: "SABBIR",
     role: 0,
-    shortDescription: { en: "Tag user with 📌 reason" },
-    longDescription: { en: "Tag a user by first name and show 📌 reason" },
+    shortDescription: { en: "Tag user with  reason" },
+    longDescription: { en: "Tag a user by first name and show  reason" },
     category: "box chat",
-    guide: { en: "{p}tag <user first name> 📌<reason>" }
+    guide: { en: "{p}tag <user first name> <reason>" }
   },
 
   onStart: async function ({ event, message, args, api }) {
     const threadID = event.threadID;
 
     if (args.length < 2)
-      return message.reply("❌ Usage: tag <name> 📌<reason>");
+      return message.reply("❌ Usage: tag <name> <reason>");
 
     const info = await api.getThreadInfo(threadID);
     const members = info.userInfo;
 
     const fullText = args.join(" ");
 
-    // Split by 📌
-    const parts = fullText.split("📌").map(t => t.trim());
+    // Split by 
+    const parts = fullText.split.map(t => t.trim());
 
     if (parts.length < 2)
-      return message.reply("❌ Please use 📌 before reason.");
+      return message.reply("❌ Please use  before reason.");
 
     const nameInput = parts[0].toLowerCase();
     const reason = parts[1] || "No reason provided";
@@ -39,7 +39,7 @@ module.exports = {
       return message.reply("❌ No user found with that name.");
 
     return message.reply({
-      body: `@${match.name}\n📌 Reason: ${reason}`,
+      body: `@${match.name}\n Reason: ${reason}`,
       mentions: [{ id: match.id, tag: `@${match.name}` }]
     });
   }
